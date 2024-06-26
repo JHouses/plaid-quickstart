3d131a0a4a4cfcdf77dd15c4d4cac26848a5f274
JHouses <casasx12@hotmail.com>
2024-06-25 19:36:50 -0500

    refactor: new UI for money advisor

--- frontend/src/Components/MoneyAdvisor/index.tsx
+++ frontend/src/Components/MoneyAdvisor/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import ProductTypesContainer from "../ProductTypes/ProductTypesContainer";
 import { ErrorDataItem, ScoreData } from "../../dataUtilities";
 import { getScoreFromItem } from "../../utils/getScore";
 
@@ -29,28 +28,32 @@ const MoneyAdvisor = () => {
     getData();
   }, []);
 
+  const scorePartParseInt = (score: string) => parseInt(score, 10);
+
   return (
-    <ProductTypesContainer productType="Fiscal Responsibility Score">
+    <div className={styles.advisorContainer}>
       {isLoading ? (
-        <h6> We are loading your score... </h6>
-      ) : error ? (
-        <h6> Unfortunately an error occurred, please try again later. </h6>
+        <h6 className={styles.adviceDescription}> We are loading your score... </h6>
+      ) : error || !data ? (
+        <h6 className={styles.adviceDescription}> Unfortunately an error occurred, please try again later. </h6>
       ) : (
         <>
-          <div className={styles.container}>
-            <div className={styles.scoreContainer}>
-              <h3>Your Score</h3>
-              <h2>{data?.score}%</h2>
-            </div>
-            <h5>Based on dates: {data?.dates.startDate} - {data?.dates.endDate}</h5>
+          <h4 className={styles.advisorTitle}>Your Result</h4>
+          <div className={styles.advisorScoreContainer}>
+            <p className={styles.advisorScore}>{scorePartParseInt(data?.score)}</p>
+            <p className={styles.advisorTotal}>of 100%</p>
           </div>
+          <p className={styles.advisorDates}>
+            <strong>Based on dates: </strong>
+            {data?.dates.startDate} - {data?.dates.endDate}
+          </p>
           <div className={styles.adviceContainer}>
-            <h3>Advice:</h3>
-            <h4>{data?.advice}</h4>  
+            <h3 className={styles.adviceTitle}>Advice:</h3>
+            <h5 className={styles.adviceDescription}>{data?.advice}</h5>  
           </div>
         </>
       )}
-    </ProductTypesContainer>
+    </div>
   );
 };
 
